71d3e899479867637523ebd2a6567a84995f4973
Nivaldo <npfs1998@yahoo.com.br>
2022-07-28 13:00:41 -0300

    Terceiro commit Livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -9,7 +9,43 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
 
+        Livro livro = new Livro("Maratona", 86);
 
         System.out.println(gato);
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private  Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
