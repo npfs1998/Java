0a0f47c9c9ae5b34c4bab412e6f9e116c596673f
Nivaldo <npfs1998@yahoo.com.br>
2022-07-28 12:42:32 -0300

    Primeiro commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        System.out.println("Aloh! " + (a+b));
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
---
+++ src/com/dio/model/Gato.java
@@ -0,0 +1,64 @@
+package com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private int idade;
+
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor, int idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return idade == gato.idade && Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
